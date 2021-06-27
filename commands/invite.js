7bc6672bcaf69d5db9e0edeca6054e5c2ad39a9a
pixel-yo <83309151+pixel-yo@users.noreply.github.com>
2021-06-27 21:46:24 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,1 +1,13 @@
-
+const Discord = require('discord.js')
+ 
+module.exports = {
+    run: message => {
+        message.channel.send(new Discord.MessageEmbed()
+            .setTitle("**__Voici le lien d'invitation du bot__**")
+            .setDescription("")
+            .setColor('RANDOM')
+            .setFooter("Si vous avez un quelconque problème avec le lien, veuillez faire la commande *support")
+            .setTimestamp())
+    },
+    name: 'help'
+}
