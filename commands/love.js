9e3023c5bf761bcffa54e820020e98c1604baa2c
pixel-yo <83309151+pixel-yo@users.noreply.github.com>
2021-06-27 16:41:17 +0200

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js'),
-    replies = ['1/10', '2/10', '3/10', '4/10', '5/10', '6/10', '7/10', '8/10', '9/10', '10/10']
+    replies = ['0%', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%','90%', '100%']
  
 module.exports = {
     run: (message, args) => {
         const question = args.join(' ')
-        if (!question) return message.channel.send('Veuillez mentionner un membre')
+        if (!question) return message.channel.send('Veuillez mentionner 2 membres')
         message.channel.send(new Discord.MessageEmbed()
             .setTitle(question)
             .setColor('RANDOM')
             .setDescription(replies[Math.floor(Math.random() * replies.length)]))
     },
-    name: 'note'
+    name: 'love'
 }
