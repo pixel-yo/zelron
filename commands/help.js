bf8a989082ad9e23d16d5f53ced768b9cae05501
pixel-yo <83309151+pixel-yo@users.noreply.github.com>
2021-06-27 15:42:50 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports = {
     run: message => {
         message.channel.send(new Discord.MessageEmbed()
             .setTitle('**__Toutes les commandes du bot__**')
-            .setDescription('⚔**__Modération__**\n\n *ban : banni le membre mentionné\n *tempban : banni temporairement le membre mentionné\n *mute : mute le membre mentionné\n *tempmute : mute temporairement le membre mentionné\n *kick : expulse le membre mentionné\n *unmute : démute la personne mentionné\n *unban : débanni la personne mentionné\n *clear + (nombre de messages): supprime le nombre de messages demandé\n')
+            .setDescription("⚔**__Modération__**\n\n *ban : banni le membre mentionné\n *tempban : banni temporairement le membre mentionné\n *mute : mute le membre mentionné\n *tempmute : mute temporairement le membre mentionné\n *kick : expulse le membre mentionné\n *unmute : démute la personne mentionné\n *unban : débanni la personne mentionné\n *clear + (nombre de messages): supprime le nombre de messages demandé\n *warn : met un avertissement à un membre\n *unwarn (beta) : supprime l'avertissement d'un membre\n")
             .setColor('RANDOM')
             .setAuthor('Commandes de Zelron')
             .setFooter('Zelron')
