cdf01e7a369044a0884341b76fdf4fe3eee4c8a4
Pixel. YO <83309151+pixel-yo@users.noreply.github.com>
2021-06-28 21:31:23 +0200

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,1 +1,13 @@
-
+const Discord = require('discord.js')
+ 
+module.exports = {
+    run: message => {
+        message.channel.send(new Discord.MessageEmbed()
+            .setTitle('**__Reverse__**')
+            .setDescription("https://tenor.com/view/reverse-card-uno-uno-cards-gif-13032597")
+            .setColor('RANDOM')
+            .setFooter('Zelron')
+            .setTimestamp())
+    },
+    name: 'help'
+}
