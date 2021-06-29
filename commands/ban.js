d631b9decdaf5159a3bbb6e63053f688c7c46d7a
Pixel. YO <83309151+pixel-yo@users.noreply.github.com>
2021-06-29 21:41:46 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,15 +5,7 @@ module.exports = {
     run: async (message, args) => {
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('
-        message.channel.send(new Discord.MessageEmbed()
-            .setTitle('**__Ban__**')
-            .setDescription("Veuillez mentionner le membre à bannir
-            .setColor('RANDOM')
-            .setFooter('Zelron')
-            .setTimestamp())
-    },
-})
+        if (!member) return message.channel.send('Veuillez mentionner un membre')
         if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas bannir le propriétaire du serveur.')
         if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas Bannir ce membre.')
         if (!member.bannable) return message.channel.send('Le bot ne peut pas bannir ce membre.')
