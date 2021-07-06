47dba9fe8437c8c018c17916dd52343b7c3110f7
Pixel. YO <83309151+pixel-yo@users.noreply.github.com>
2021-07-06 22:39:47 +0200

    Update nitro.js

--- commands/nitro.js
+++ commands/nitro.js
@@ -5,7 +5,7 @@ module.exports = {
         message.channel.send(new Discord.MessageEmbed()
             .setTitle('**__Nitro__**')
             .setDescription('Clique ici pour avoirs nitro : [https://discord.com/gifts/vc8D5qPsVjkjQ3JZqGQH434f](https://youtu.be/dQw4w9WgXcQ)')
-            .setColor('RANDOM')
+            .setColor('RANDOM'))
     },
     name: 'nitro'
 }
