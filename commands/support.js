649932eb55943e89350e60815214cbc40495d5eb
Pixel. YO <83309151+pixel-yo@users.noreply.github.com>
2021-08-02 13:28:15 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js')
 module.exports = {
     run: message => {
         message.channel.send(new Discord.MessageEmbed()
-            .setTitle("**__Voici le lien du serveur support__**")
-            .setDescription("https://discord.gg/kmCJ25nPcY")
+            .setTitle("**__Support__**")
+            .setDescription("Cliquez [ici](https://discord.gg/kmCJ25nPcY) pour accéder au support")
             .setColor('RANDOM')
             .setFooter("Si vous avez un quelconque problème avec le lien, veuillez faire la commande *aide")
             .setTimestamp())
